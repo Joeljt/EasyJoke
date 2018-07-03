461e89766ecc92165eafa71439b1623ffe647d4e
joeljt <joseph_l@126.com>
2018-07-03 17:48:13 +0800

    AlertDialog

--- BaseLibrary/src/main/java/com/ljt/baselibrary/dialog/AlertController.java
+++ BaseLibrary/src/main/java/com/ljt/baselibrary/dialog/AlertController.java
@@ -4,8 +4,13 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.util.SparseArray;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Created by lijiateng on 2018/7/2.
@@ -14,6 +19,8 @@ class AlertController {
 
     private AlertDialog mDialog;
     private Window mWindow;
+
+    private DialogViewHelper mViewHelper;
 
     public AlertController(AlertDialog dialog, Window window) {
         this.mDialog = dialog;
@@ -26,6 +33,22 @@ class AlertController {
 
     public Window getWindow() {
         return mWindow;
+    }
+
+    public void setViewHelper(DialogViewHelper mViewHelper) {
+        this.mViewHelper = mViewHelper;
+    }
+
+    public void setText(int viewId, CharSequence text) {
+        mViewHelper.setText(viewId, text);
+    }
+
+    public void setOnClickListener(int viewId, View.OnClickListener listener) {
+        mViewHelper.setOnClickListener(viewId, listener);
+    }
+
+    public <V extends View> V getView(int viewId) {
+        return mViewHelper.getView(viewId);
     }
 
     public static class AlertParams{
@@ -65,6 +88,19 @@ class AlertController {
          */
         public SparseArray<View.OnClickListener> mClickArray = new SparseArray<>();
 
+        /**
+         * 默认宽度
+         */
+        public int mWidth = ViewGroup.LayoutParams.WRAP_CONTENT;
+        public int mHeight = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+        public int mGravity = Gravity.CENTER;
+
+        /**
+         * 默认的
+         */
+        public int mAnimation = 0;
+
         public AlertParams(Context context, int themeResId){
             this.mContext = context;
             this.mThemeResId = themeResId;
@@ -95,17 +131,29 @@ class AlertController {
             // 设置布局
             mAlert.getDialog().setContentView(mViewHelper.getContentView());
 
+            // 设置 ViewHelper
+            mAlert.setViewHelper(mViewHelper);
+
             // 2.设置文本展示
             for (int i = 0; i < mTextArray.size(); i++) {
-                mViewHelper.setText(mTextArray.keyAt(i), mTextArray.valueAt(i));
+                mAlert.setText(mTextArray.keyAt(i), mTextArray.valueAt(i));
             }
 
             // 3.设置点击事件
             for (int i = 0; i < mClickArray.size(); i++) {
-                mViewHelper.setOnClickListener(mClickArray.keyAt(i), mClickArray.valueAt(i));
+                mAlert.setOnClickListener(mClickArray.keyAt(i), mClickArray.valueAt(i));
             }
 
             // 4.配置自定义效果
+            Window window = mAlert.getWindow();
+            window.setGravity(mGravity);
+            if (mAnimation != 0) {
+                window.setWindowAnimations(mAnimation);
+            }
+            WindowManager.LayoutParams layoutParams = window.getAttributes();
+            layoutParams.width = mWidth;
+            layoutParams.height = mHeight;
+            window.setAttributes(layoutParams);
 
         }
 
--- BaseLibrary/src/main/java/com/ljt/baselibrary/dialog/AlertDialog.java
+++ BaseLibrary/src/main/java/com/ljt/baselibrary/dialog/AlertDialog.java
@@ -4,7 +4,10 @@ import android.app.Dialog;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ljt.baselibrary.R;
 
@@ -24,6 +27,17 @@ public class AlertDialog extends Dialog {
 
     }
 
+    public void setText(int viewId, CharSequence text) {
+        mAlert.setText(viewId, text);
+    }
+
+    public void setOnClickListener(int viewId, View.OnClickListener listener) {
+        mAlert.setOnClickListener(viewId, listener);
+    }
+
+    public <V extends View> V getView(int viewId) {
+        return mAlert.getView(viewId);
+    }
 
     public static class Builder{
 
@@ -88,6 +102,34 @@ public class AlertDialog extends Dialog {
          */
         public Builder setOnKeyListener(OnKeyListener onKeyListener) {
             P.mOnKeyListener = onKeyListener;
+            return this;
+        }
+
+        /**
+         * 是否横向充满屏幕
+         * @return
+         */
+        public Builder fullWidth() {
+            P.mWidth = ViewGroup.LayoutParams.MATCH_PARENT;
+            return this;
+        }
+
+        /**
+         * 从底部弹出
+         * @param isAnimation 是否有动画
+         * @return
+         */
+        public Builder fromBottom(boolean isAnimation){
+            if(isAnimation){
+                P.mAnimation = R.style.dialog_from_bottom_anim;
+            }
+            P.mGravity = Gravity.BOTTOM;
+            return this;
+        }
+
+        public Builder setWidthAndHeight(int width, int height) {
+            P.mWidth = width;
+            P.mHeight = height;
             return this;
         }
 
